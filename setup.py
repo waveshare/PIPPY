da3db26029c00ba2ded72006afa8af9ebd6551ae
waveshare <36142105+waveshare@users.noreply.github.com>
2022-02-19 16:19:47 +0800

    Update setup.py
    Update for supporting Raspberry Pi Bullseye OS

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-# File name   : setup.py
+# File name   : setup.py for PIPPY
 # Date        : 2020/11/24
 
 import os
 import time
+import re
 
 curpath = os.path.realpath(__file__)
 thisPath = os.path.dirname(curpath)
@@ -20,7 +21,15 @@ def replace_num(file,initial,new_num):
         f.writelines(newline)
 
 for x in range(1,4):
-	if os.system("sudo apt-get update") == 0:
+	if os.system("sudo apt update") == 0:
+		break
+
+for x in range(1,4):
+	if os.system("sudo apt -y dist-upgrade") == 0:
+		break
+
+for x in range(1,4):
+	if os.system("sudo apt clean") == 0:
 		break
 
 for x in range(1,4):
@@ -28,7 +37,7 @@ for x in range(1,4):
 		break
 
 for x in range(1,4):
-	if os.system("sudo apt-get install -y python-dev python-pip libfreetype6-dev libjpeg-dev build-essential") == 0:
+	if os.system("sudo apt-get install -y python-dev python3-pip libfreetype6-dev libjpeg-dev build-essential") == 0:
 		break
 
 for x in range(1,4):
@@ -60,34 +69,60 @@ for x in range(1,4):
 		break
 
 try:
-	replace_num("/boot/config.txt",'#dtparam=i2c_arm=on','dtparam=i2c_arm=on\nstart_x=1')
+	replace_num("/boot/config.txt",'#dtparam=i2c_arm=on','dtparam=i2c_arm=on')
+except:
+	print('try again')
+
+try:
+	replace_num("/boot/config.txt",'[all]','[all]\ngpu_mem=128')
+except:
+	print('try again')
+
+try:
+	replace_num("/boot/config.txt",'camera_auto_detect=1','#camera_auto_detect=1\nstart_x=1')
+except:
+	print('try again')
+
+try:
+	replace_num("/boot/config.txt",'camera_auto_detect=1','#camera_auto_detect=1')
 except:
 	print('try again')
 
 
 for x in range(1,4):
-	if os.system("sudo pip3 install numpy") == 0:
+	if os.system("sudo pip3 install opencv-contrib-python==3.4.11.45") == 0:
+		break
+	elif os.system("sudo pip3 install -i http://pypi.douban.com/simple/ --trusted-host=pypi.douban.com/simple opencv-contrib-python==3.4.11.45") == 0:
 		break
 
 for x in range(1,4):
-	if os.system("sudo pip3 install opencv-contrib-python==3.4.3.18") == 0:
+	if os.system("sudo pip3 uninstall -y numpy") == 0:
 		break
 
 for x in range(1,4):
-	if os.system("sudo apt-get -y install libqtgui4 libhdf5-dev libhdf5-serial-dev libatlas-base-dev libjasper-dev libqt4-test") == 0:
+	if os.system("sudo pip3 install numpy==1.21") == 0:
+		break
+	elif os.system("sudo pip3 install -i http://pypi.douban.com/simple/ --trusted-host=pypi.douban.com/simple numpy==1.21") == 0:
+		break
+
+for x in range(1,4):
+	if os.system("sudo apt-get -y install libhdf5-dev libhdf5-serial-dev libatlas-base-dev libjasper-dev") == 0:
 		break
 
 for x in range(1,4):
 	if os.system("sudo pip3 install imutils zmq pybase64 psutil") == 0:
 		break
+	elif os.system("sudo pip3 install -i http://pypi.douban.com/simple/ --trusted-host=pypi.douban.com/simple imutils zmq pybase64 psutil") == 0:
+		break
+
+for x in range(1,4):
+	if os.system("sudo apt-get install -y util-linux procps hostapd iproute2 iw haveged dnsmasq") == 0:
+		break
 
 for x in range(1,4):
 	if os.system("sudo pip3 install pi-ina219") == 0:
 		break
-        
-for x in range(1,4):
-	if os.system("sudo apt-get install -y util-linux procps hostapd iproute2 iw haveged dnsmasq") == 0:
-		break
+
         
 for x in range(1,4):
 	if os.system("cd " + thisPath + " && cd .. && sudo git clone https://github.com/oblique/create_ap") == 0:
@@ -101,3 +136,5 @@ except:
 replace_num('/etc/rc.local','exit 0','cd '+thisPath+' && sudo python3 webServer.py &\nexit 0')
 
 print('Completed!')
+
+os.system("sudo reboot")
